e4bd076401d48d1044a3b843b5cb3a042b373469
Ranveer-Sule <ranveer.sule@education.nsw.gov.au>
2026-03-07 18:20:49 +1100

    LED and button control logic in main.py; update diagram.json for new components

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from machine import Pin
 
-led = Pin(13, Pin.OUT)
+led1 = Pin(13, Pin.OUT)
+led2 = Pin(12, Pin.OUT)
+button = Pin(17, Pin.IN)
 
-led.value(1)
+while True:
+  while button.value() == 1:
+    led1.value(1)
+    led2.value(1)
+  else:
+    led1.value(0)
+    led2.value(0)
