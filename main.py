a0575aec0ba7fdcc114914b96e20b3c4cd84a0d7
Ranveer-Sule <ranveer.sule@education.nsw.gov.au>
2026-03-08 23:37:53 +1100

    added buzzer control logic in main.py; update diagram.json for new components

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from machine import Pin
+from machine import Pin,PWM
 
-led1 = Pin(13, Pin.OUT)
-led2 = Pin(12, Pin.OUT)
-button = Pin(17, Pin.IN)
+green_led = Pin(13, Pin.OUT)
+red_led = Pin(12, Pin.OUT)
+button = Pin(17, Pin.IN, Pin.PULL_DOWN)
+buzzer = PWM(Pin(16))
 
 while True:
-  while button.value() == 1:
-    led1.value(1)
-    led2.value(1)
-  else:
-    led1.value(0)
-    led2.value(0)
+    if button.value() == 1:
+        green_led.value(1)
+        red_led.value(1)
+        buzzer.freq(1000)
+        buzzer.duty_u16(30000)
+    else:
+        green_led.value(0)
+        red_led.value(0)
+        buzzer.duty_u16(0)
